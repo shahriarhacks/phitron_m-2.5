582ab5f2194a8b645dbcf191bb614a41555b6e2a
SHAHRIAR AHMED <shahriar7ahmed@gmail.com>
2023-04-04 09:22:42 +0600

    UnComment

--- practice.c
+++ practice.c
@@ -1,58 +1,59 @@
 #include<stdio.h>
 
 int main(){
+    // Run every single problem if you run all at a time it will show you wrong answer or error
     //problem-1
 
-    //printf("Recently I heard that you've achieved 95%% marks in exam.\nThis is brilliant!\nI wish you'll shine your life!\tGood luck with all the barriers(/\\) in your life" );
+    printf("Recently I heard that you've achieved 95%% marks in exam.\nThis is brilliant!\nI wish you'll shine your life!\tGood luck with all the barriers(/\\) in your life" );
 
 
     //problem-2
 
-    // int num1,num2;
+    int num1,num2;
 
-    // scanf("%d %d",&num1,&num2);
+    scanf("%d %d",&num1,&num2);
 
-    // int sum= num1+num2;
-    // int sub= num1-num2;
-    // int mul=num1*num2;
-    // float div=num1*1.0/num2;
-    // int mod=num1%num2;
+    int sum= num1+num2;
+    int sub= num1-num2;
+    int mul=num1*num2;
+    float div=num1*1.0/num2;
+    int mod=num1%num2;
 
-    // printf("%d %d %d %0.2f %d",sum,sub,mul,div,mod);
+    printf("%d %d %d %0.2f %d",sum,sub,mul,div,mod);
 
     // problem-3
 
-    // int num;
+    int num;
 
-    // scanf("%d",&num);
-    // if(num>0){
-    //     if((num%2)==0){
-    //     printf("Number is even");
-    //     }
-    //     else{
-    //         printf("Number is odd");
-    //     }
+    scanf("%d",&num);
+    if(num>0){
+        if((num%2)==0){
+        printf("Number is even");
+        }
+        else{
+            printf("Number is odd");
+        }
 
-    // }
-    // else{
-    //     printf("The Number is negative");
-    // }
+    }
+    else{
+        printf("The Number is negative");
+    }
 
 
     //problem-4
 
-    // int calcNum;
-    // scanf("%d",&calcNum);
+    int calcNum;
+    scanf("%d",&calcNum);
 
-    // if(calcNum>0){
-    //     printf("Number is Positive");
-    // }
-    // else if(calcNum<0){
-    //     printf("Number is Negative");
-    // }
-    // else{
-    //     printf("Number is ZERO");
-    // }
+    if(calcNum>0){
+        printf("Number is Positive");
+    }
+    else if(calcNum<0){
+        printf("Number is Negative");
+    }
+    else{
+        printf("Number is ZERO");
+    }
 
     //problem-5
 
